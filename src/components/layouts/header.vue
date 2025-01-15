1fc69fd522f5ac8d73eba2b83581ae68a6c734b9
ygkim97 <dnjdn21@naver.com>
2025-01-15 10:02:46 +0900

    feat: 메인 페이지 수정 및 파일 정리 - 메인 페이지 수정 - header 컴포넌트 분리 - css 파일 정리 - 안쓰는 컴포넌트 삭제 - icon 컴포넌트 assets 디렉토리로 이동

---
+++ src/components/layouts/header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from "vue-router";
+import { computed } from "vue";
+const route = useRoute();
+const currentPath = computed(() => route.path);
+console.log(currentPath.value);
+const menuList = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" }
+];
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="navbar-container">
+      <RouterLink to="/" class="navbar-logo">
+        <img alt="Vue logo" src="@/assets/images/icon/vueLogo.svg" width="25" height="25" />
+        <span class="navbar-logo-text">VUE3-COMPONENT</span>
+      </RouterLink>
+      <div class="navbar-menu" id="navbar-dropdown">
+        <ul class="navbar-menu-list">
+          <li v-for="menu in menuList">
+            <RouterLink :to="menu.path" class="navbar-menu-item" :class="{ active: currentPath === menu.path }">
+              {{ menu.name }}
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
