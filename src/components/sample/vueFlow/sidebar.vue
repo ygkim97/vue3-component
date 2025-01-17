9f2386a23b2bfb1dab353218ccafc661ae638c8d
ygkim97 <dnjdn21@naver.com>
2025-01-17 10:55:13 +0900

    feat: sidebar 내용이 overflow 된 경우, scroll 추가

--- src/components/sample/vueFlow/sidebar.vue
+++ src/components/sample/vueFlow/sidebar.vue
@@ -10,7 +10,8 @@ const contentRefs = ref([]);
 const contentHeights = ref({});
 const accordionItems = ref([
   { id: "nodeDataControls", label: "Node & Data Controls (Button)" },
-  { id: "nodeCollection", label: "Node Collection (Drag)" }
+  { id: "nodeCollection", label: "Node Collection (Drag)" },
+  { id: "scrollTest", label: "Contents Overflow Scroll Test" }
 ]);
 const accordionState = ref({});
 
@@ -42,33 +43,61 @@ onMounted(() => {
     <div class="vue-flow__sidebar-title">VUE FLOW</div>
 
     <!-- TODO: sidebar common component -->
-    <!-- TODO: handle overflow content with scroll -->
-    <div v-for="item in accordionItems" :key="`item_${item.id}`" class="vue-flow__accordion">
-      <button @click="toggleAccordion(item.id)" class="vue-flow__accordion-header">
-        <span class="vue-flow__accordion-text">{{ item.label }}</span>
-        <span class="vue-flow__accordion-icon">
-          <ArrowUpIcon v-if="accordionState[item.id]" />
-          <ArrowDownIcon v-else />
-        </span>
-      </button>
-      <div
-        :id="item.id"
-        class="vue-flow__accordion-content"
-        :style="{
-          maxHeight: accordionState[item.id] ? `${contentHeights[item.id]}px` : '0px'
-        }"
-        ref="contentRefs"
-      >
-        <div class="vue-flow__accordion-body">
-          <div v-if="item.id === 'nodeDataControls'" class="vue-flow__button-group">
-            <button type="button" @click="$emit('getJsonData')">Get JSON Data</button>
-            <button type="button" @click="$emit('createNode')">Create Node</button>
-            <button type="button" @click="$emit('resetNode')">Reset Node</button>
-          </div>
-          <div v-if="item.id === 'nodeCollection'" class="vue-flow__drag-group">
-            <div :draggable="true" @dragstart="onDragStart($event, 'input')">Input Node</div>
-            <div :draggable="true" @dragstart="onDragStart($event, 'default')">Default Node</div>
-            <div :draggable="true" @dragstart="onDragStart($event, 'output')">Output Node</div>
+    <div class="vue-flow__sidebar-contents">
+      <div v-for="item in accordionItems" :key="`item_${item.id}`" class="vue-flow__accordion">
+        <button @click="toggleAccordion(item.id)" class="vue-flow__accordion-header">
+          <span class="vue-flow__accordion-text">{{ item.label }}</span>
+          <span class="vue-flow__accordion-icon">
+            <ArrowUpIcon v-if="accordionState[item.id]" />
+            <ArrowDownIcon v-else />
+          </span>
+        </button>
+        <div
+          :id="item.id"
+          class="vue-flow__accordion-content"
+          :style="{
+            maxHeight: accordionState[item.id] ? `${contentHeights[item.id]}px` : '0px'
+          }"
+          ref="contentRefs"
+        >
+          <div class="vue-flow__accordion-body">
+            <div v-if="item.id === 'nodeDataControls'" class="vue-flow__button-group">
+              <button type="button" @click="$emit('getJsonData')">Get JSON Data</button>
+              <button type="button" @click="$emit('createNode')">Create Node</button>
+              <button type="button" @click="$emit('resetNode')">Reset Node</button>
+            </div>
+            <div v-else-if="item.id === 'nodeCollection'" class="vue-flow__drag-group">
+              <div :draggable="true" @dragstart="onDragStart($event, 'input')">Input Node</div>
+              <div :draggable="true" @dragstart="onDragStart($event, 'default')">Default Node</div>
+              <div :draggable="true" @dragstart="onDragStart($event, 'output')">Output Node</div>
+            </div>
+            <div v-else-if="item.id === 'scrollTest'">
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur dicta harum ipsam nam
+                necessitatibus perferendis quam, quia sequi veritatis? Commodi enim expedita illum inventore nisi, quam
+                reprehenderit sit voluptatibus?
+              </div>
+              <div>
+                Ab accusantium ad animi consectetur consequatur cupiditate delectus, deserunt explicabo iste, iusto
+                labore nostrum odit perspiciatis praesentium quas quidem saepe sint sit, vel voluptatem! Consectetur
+                exercitationem libero minima odio veniam.
+              </div>
+              <div>
+                A alias atque beatae, dolor doloremque dolorum, eligendi eos error expedita facilis itaque iusto maxime
+                nam nostrum pariatur possimus ratione rem repellendus reprehenderit saepe sed soluta velit! A, excepturi
+                pariatur.
+              </div>
+              <div>
+                Animi delectus dignissimos ducimus eligendi iusto laudantium non quas quisquam sit vitae. A animi
+                debitis dicta dolorem error est fugit impedit nam neque, non officia, perspiciatis porro sequi tempora
+                veniam!
+              </div>
+              <div>
+                Accusantium, ad alias at, cum deserunt ea eligendi facere illo in ipsam laboriosam magnam minus
+                necessitatibus nemo neque nobis omnis porro praesentium quae quaerat quam, quo similique sunt! Adipisci,
+                quis.
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -78,11 +107,15 @@ onMounted(() => {
 
 <style>
 .vue-flow__sidebar {
-  @apply w-[25vw] border-r-2 border-gray-200;
+  @apply w-[25vw] border-r-2 border-gray-200 grid grid-rows-[auto,1fr];
 }
 
 .vue-flow__sidebar-title {
   @apply font-bold text-center text-xl py-4 border-b-2 border-gray-200;
+}
+
+.vue-flow__sidebar-contents {
+  @apply overflow-y-scroll;
 }
 
 /* Accordion */
